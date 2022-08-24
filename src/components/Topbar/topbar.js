7a2c710389b28aeda746cc0f53ab795a12418224
Ibrahim Pala <96876777+IbrahimPala1@users.noreply.github.com>
2022-08-24 22:48:52 +0100

    added navigation menu

--- src/components/Topbar/topbar.js
+++ src/components/Topbar/topbar.js
@@ -1,13 +1,26 @@
 import './topbar.scss'
-import { Link } from 'react-router-dom' 
+import { Link, NavLink } from 'react-router-dom' 
 import LogoS from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 const Topbar = () => (
-    <div className='nav-bar'>
-        <Link className='logo' to='/'>
-            <img src={LogoS} alt='logo' />
-        </Link>
-    </div>
+  <div className="nav-bar">
+    <Link className="logo" to="/">
+      <img src={LogoS} alt="logo" />
+    </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to='/'>
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink exact="true" activeclassname="active" className="about-link" to='/'>
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink exact="true" activeclassname="active" className="contact-link" to='/contact'>
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+  </div>
 )
 
 export default Topbar
