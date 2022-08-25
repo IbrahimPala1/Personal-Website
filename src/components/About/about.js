85b4f59a52ae11e8a38bc576b34172ab9f21e2a9
Ibrahim Pala <96876777+IbrahimPala1@users.noreply.github.com>
2022-08-25 18:24:52 +0100

    about me page edits

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,3 +1,28 @@
-const About = () => ('Hello')
+import AnimatedLetters from '../AnimatedLetters/letters'
+import './about.scss'
+
+const About = () => {
+    return (
+        <div className="container about-page">
+            <div className="text-zone">
+            <h1>
+                <AnimatedLetters
+                strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+                idx={15}
+                />
+            </h1>
+            <p>
+                Paragraph
+            </p>
+            <p align="LEFT">
+                Paragraph
+            </p>
+            <p>
+                Paragraph
+            </p>
+            </div>
+            </div>
+    )
+}
 
 export default About 
