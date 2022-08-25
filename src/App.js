c72a095ef871fdbc30233e30df6626076a3149d1
Ibrahim Pala <96876777+IbrahimPala1@users.noreply.github.com>
2022-08-25 19:14:39 +0100

    initial contact me page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/layout';
 import Home from './components/Home/home'
 import About from './components/About/about'
+import Contact from './components/Contact/contact'
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
               <Route path='/' element={<Layout />} >
                 <Route index element ={<Home/>} />
                 <Route path='about' element ={<About/>} />
+                <Route path='contact' element ={<Contact/>} />
               </Route>
           </Routes> 
         </>
